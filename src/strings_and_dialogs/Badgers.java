5cf589a04cd30e68a225aa42f8200181b583d21a
sophie-z <League@AdministorsiMac.attlocal.net>
2019-01-05 11:25:52 -0800

    MyAges, Badgers IP

---
+++ src/strings_and_dialogs/Badgers.java
@@ -0,0 +1,17 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class Badgers {
+public static void main(String[] args) {
+	String b="badger,";
+	String m="mushroom,";
+	String s="a snake,";
+	for(int i=0; i<10; i++) {System.out.println(b);}
+	for(int g=0; g<2; g++) {System.out.println(m);}
+	for(int t=0; t<2; t++) { System.out.println(s);
+	}
+
+	
+	}
+}
---
+++ src/strings_and_dialogs/MyAges.java
@@ -0,0 +1,13 @@
+package strings_and_dialogs;
+
+import javax.swing.JOptionPane;
+
+public class MyAges {
+	public static void main(String[] args) {
+	
+	for(int i=0; i<11; i++) {
+		System.out.println(i);  	
+	}
+	}
+}
+
